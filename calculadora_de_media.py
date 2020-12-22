6e439c729e662675be28765838ec5d520e835a3d
Gabriel Romao <romaoplays@hotmail.com>
2020-12-22 12:08:29 -0300

    Overhauled entire code from google sheets to csv

--- calculadora_de_media.py
+++ calculadora_de_media.py
@@ -1,100 +1,85 @@
-import ezsheets
+import csv
+import logging
 
-ss = ezsheets.Spreadsheet("1AP3ndHUSytLDzKFvT2V3Je_zO2dl1uzHf_RvjG70ew4")
-sheet = ss[0]
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s -  %(levelname)s -  %(message)s"
+)
+logging.disable(logging.CRITICAL)
 
-materias = sheet.getColumn(1)
-# avaliacoes_1 = sheet.getColumn(2)
-# pesos_1 = sheet.getColumn(3)
-# avaliacoes_2 = sheet.getColumn(4)
-# pesos_2 = sheet.getColumn(5)
-# avaliacoes_3 = sheet.getColumn(6)
-# pesos_3 = sheet.getColumn(7)
-# avaliacoes_4 = sheet.getColumn(8)
-# pesos_4 =  sheet.getColumn(9)
-# avaliacoes_5 = sheet.getColumn(10)
-# pesos_5 =  sheet.getColumn(11)
+try:
+    csv_file = open("media.csv")
+except FileNotFoundError:
+    csv_file = open("media.csv", "w")
+    csv_file.close()
+    csv_file = open("media.csv", "r")
 
-tamanho_materias = []
+csv_reader = csv.reader(csv_file)
 
-for i in range(len(materias)):
-    if materias[i] != "":
-        tamanho_materias.append(materias[i])
-
-# Perguntar Qual média quer calcular/Se quer adicionar uma nova
-print("Qual média quer calcular?")
-print("0 - Adicionar uma nova")
-for i in range(len(tamanho_materias)):
-    print(str(i + 1) + " - " + materias[i])
-
-# Adicionar uma nova e salvar no EZSHEETS #Nome da matéria | avaliação | peso | avaliação | peso |
-j = 1
-g = 0
+csv_list = list(csv_reader)
 
 
-def pegar_resposta():
-    global j
-    resposta_nome = input()
-    if resposta_nome == "salvar":
-        return False
-    else:
-        sheet[j, len(tamanho_materias) + 1] = resposta_nome
-        j = j + 1
-        return True
+print("\nQual média você quer calcular?\n\na - Adicionar Matéria\n")
+logging.debug(len(csv_list))
+csv_list_temp = []
+for row in csv_list:
+    if any(row):
+        csv_list_temp.append(row)
+csv_list = csv_list_temp
 
+if range(len(csv_list)) != 0:
+    for i in range(len(csv_list)):
+        print(f"{str(i)} - {csv_list[i][0]}")
 
 resposta = input()
-resposta = int(resposta)
-if resposta == 0:
-    print('Qual o nome da matéria? (Escreva "salvar" para salvar a qualquer momento)')
-    if pegar_resposta() == False:
-        g = 1
-    for i in range(10):
-        if g == 1:
-            break
-        print(
-            "Qual o nome da "
-            + str(i + 1)
-            + 'a avaliação? (Escreva "salvar" para salvar)'
-        )
-        if pegar_resposta() == False:
-            break
-        print("Qual o peso da prova?")
-        while True:
-            if pegar_resposta() == False:
-                print("Por favor adicione um peso")
-            else:
-                break
-    print("Resposta adicionada")
-    g = 0
 
-# Calcular a média
+if resposta == "a":
+    csv_file.close()
+    logging.debug("File closed")
+    csv_file = open("media.csv", "a")
+    logging.debug("File opened as write")
+    lista_add = []
+    print("Nome da matéria:")
+    lista_add.append(input())
+    k = 1
+    while True:
+        print(f"Nome da {k}a avaliação: ('fim' para sair)")
+        input_avaliacao = input()
+        if input_avaliacao == "fim":
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(lista_add)
+            logging.debug("File closed")
+            break
+        else:
+            lista_add.append(input_avaliacao)
+            print(f"Peso da {k}a avaliação:")
+            lista_add.append(input())
+        k = k + 1
 else:
-    rows = sheet.getRow(resposta)
-    rows_tamanho = []
-    for i in range(len(rows)):
-        if rows[i] != "":
-            rows_tamanho.append(rows[i])
+    media_list = csv_list[int(resposta)]
+    nota_list = []
+    peso_list = []
+    media_soma = 0
+    peso_soma = 0
+    for i in range(len(media_list)):
+        if i == 0:
+            pass
+        elif (i % 2) == 1:
+            print(f"Nota de {media_list[i]}:")
+            nota_list.append(float(input()))
+        else:
+            peso_list.append(float(media_list[i]))
 
-    lista_notas = []
+    for i in range(len(nota_list)):
+        media_soma = (nota_list[i] * peso_list[i]) + media_soma
+        peso_soma = peso_list[i] + peso_soma
 
-    for i in range(1, len(rows_tamanho), 2):
-        print("Nota da " + str(rows[i]))
-        resposta_2 = input()
-        lista_notas.append(resposta_2)
+    media_final = media_soma / peso_soma
+    print(f"\nSua média final é: {round(media_final,2)}")
 
-    lista_final = []
-    lista_pesos = []
-    p = 0
 
-    for i in range(2, len(rows_tamanho), 2):
-        lista_final.append(float(lista_notas[p]) * float(rows[i]))
-        lista_pesos.append(float(rows[i]))
-        p = p + 1
-
-    media_final = float(sum(lista_final)) / float(sum(lista_pesos))
-
-    print("Sua média final é: " + str(round(media_final, 1)))
+csv_file.close()
 
 while True:
-    nadaa = input()
+    print("\n'sair' para sair")
+    if input() == "sair":
+        break
